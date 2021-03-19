b4856fb74b15b794793674ba135b093605d25a31
jabyz <javiernba@gmail.com>
2021-03-19 12:39:32 -0600

    Initial Commit

---
+++ get-windows-username-api-net-core/get-windows-username-api-net-core/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace get_windows_username_api_net_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        
+        [HttpGet]
+        //GET : /api/Account
+        public JsonResult Get()
+        {
+            IPrincipal p = HttpContext.User;
+            return new JsonResult(new { UserName = p.Identity.Name });
+        }
+    }
+}
